492bff9d225f8e63ebababb42eed82262a42fab6
Arnold Raphel <arnoldrnld101@gmail.com>
2026-06-24 00:48:51 +0530

    chore(ecr): force_delete=true so destroy can remove repos with images (clean rebuilds)

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -3,6 +3,8 @@ resource "aws_ecr_repository" "this" {
 
   name                 = each.value
   image_tag_mutability = "IMMUTABLE"
+  # Allow `terraform destroy` to remove repos that still contain images (non-prod teardown/rebuild).
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
